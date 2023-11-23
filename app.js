797a2318b5e75c5824d1a1ae34e4b465996f59f9
Azat Garipov <149225941+forget89@users.noreply.github.com>
2023-11-23 17:46:31 +0500

    popup added, just popup

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
-const test = 'test'
+const buttonElem = document.querySelector('.right-plate__footer');
+const popupElem = document.querySelector('.right-plate__popup');
 
-var btn = document.getElementById("button")
-function btnactive(btn) {
-    console.log(test)
+popupElem.style.cssText = `
+    display: flex;
+    visibility: hidden;
+    opacity: 0;
+    transition: opacity 300ms ease-in-out;
+`;
+
+const closePopup = Event => {
+    const target = event.target;
+
+    if(target === popupElem || target.closest('.popup-container__close')) {
+
+    popupElem.style.opacity = 0;
+
+    setTimeout(() => {
+    popupElem.style.visibility = 'hidden';
+    },300)
+    }
 }
+
+const openPopup = () => {
+    popupElem.style.visibility = 'visible';
+    popupElem.style.opacity = 1;
+};
+
+buttonElem.addEventListener('click', openPopup);
+popupElem.addEventListener('click', closePopup)
